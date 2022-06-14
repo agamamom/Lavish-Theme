550f5c0b7e4a0c8f1b731d3f61bde5f6aecd71a0
Dat <dathoang31082001@gmail.com>
2022-06-14 20:18:55 +0700

    finish project

--- src/App.js
+++ src/App.js
@@ -8,7 +8,9 @@ import './App.css';
 import Navbar from './components/Navbar';
 import Footer from './components/pages/Footer/Footer';
 import Home from './components/pages/HomePage/Home';
-
+import Services from './components/pages/Services/Services';
+import Products from './components/pages/Products/Products';
+import SignUp from './components/pages/SignUp/SignUp';
 
 function App() {
   return (
@@ -16,6 +18,9 @@ function App() {
       <Navbar/>
       <Routes> 
         <Route path='/' element={<Home/>} />
+        <Route path='/services' element={<Services/>} />
+        <Route path='/products' element={<Products/>} />
+        <Route path='/sign-up' element={<SignUp/>} />
       </Routes>
       <Footer/>
     </Router>
